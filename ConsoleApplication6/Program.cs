c7c205cc59606e3f97d2ba97c6d296104d6b722c
Your Name <you@example.com>
2015-09-27 19:10:16 -0400

    6

---
+++ ConsoleApplication6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            { Console.WriteLine("What is your height?");
+               }
+            Double h = Convert.ToDouble(Console.ReadLine());
+
+            
+
+            { Console.WriteLine("What is your age?");
+               }
+           Double a = Convert.ToDouble(Console.ReadLine());
+
+            if
+                (h  5.0 && a >= 17)
+
+            { Console.WriteLine("Ok"); }
+
+            else
+            { Console.WriteLine("NO!!!"); }
+
+                    
+
+        }
+    }
+}
